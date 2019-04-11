15a06d3abca54240c9ac69cf5f91348e1ea120d6
Ivan Wong <ivangundampc@gmail.com>
2019-04-11 19:43:23 +0800

    Implemented adding item into cart, others left for you to do

--- js/Cart.js
+++ js/Cart.js
@@ -2,6 +2,9 @@
 // load the body first before creating the Cart object
 // written by Ken
 
+var url = window.location.pathname;
+var filename = url.substring(url.lastIndexOf('/') + 1);
+
 // use getElementsByClassName() method to get a collection of elements having the name 'cart-remove-btn' in the page 
 // store all these elements in the array referenced by variable removeBtns
 var removeBtns = document.getElementsByClassName('cart-remove-btn');
@@ -23,10 +26,33 @@ for (var i = 0; i<addBtns.length; i++) {
     addBtn.addEventListener('click', fetchData); // fetch the data from the store after "Add to Cart" button is clicked
 }
 
+/**
+ * cartItems is an array includes object that contains item information in the cart, and the element in cartItems is look like: 
+ * const item = {
+        imageSrc,
+        brandName,
+        productName,
+        price
+    } 
+*/ 
+var cartItems = JSON.parse(localStorage.getItem("CART_ITEMS"));
+
+// If it is null, which means CART_ITEMS is never stored, initialize it with an array
+if (cartItems == null) {
+    cartItems = []
+}
+
+// If user is in the cart page, run reload the generate the rows in the cart table
+if (filename === 'cart.html') {
+    reload();
+}
+
 function removeItem(event) {
-    var removeClicked = event.target; // the remove button clicked is the event.target (object that triggers the event)
-    removeClicked.parentElement.parentElement.parentElement.remove(); // remove the whole table row
-    updateTotal(); // update total price of all the merchandise with the function updateTotal()
+    /**
+     * TODO: when removeItem(event) is clicked, you need to remove the corresponding item in cartItems
+     * Then, you can call reload() and the new table will be generated
+     * So, what you need to do is to find out the removeButton clicked is belong to which item and remove it in cartItems, then all things solve!
+     */
 }
 
 function qtyValid(event) {
@@ -46,18 +72,30 @@ function fetchData(event) {
     var brandName = shopItem.getElementsByClassName('brand-name')[0].innerText;
     var productName = shopItem.getElementsByClassName('product-name')[0].innerText;
     var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText;
+
+    const item = {
+        imageSrc,
+        brandName,
+        productName,
+        price
+    }
+
+    cartItems.push(item)
+    localStorage.setItem("CART_ITEMS", JSON.stringify(cartItems));
+
     // console.log(imageSrc, brandName, productName, price);
-    addItem(imageSrc, brandName, productName, price);
+    // addItem(imageSrc, brandName, productName, price);
 }
 
-function addItem(imageSrc, brandName, productName, price) {
+function generateCartItem(item) {
     var cartRow = document.createElement('tr');
+    cartRow.className = "cart-row"
     var cartRowContents = `
     <td class="cart-item">
-        <img class="cart-item-image" src="${imageSrc}">
+        <img class="cart-item-image" src="${item.imageSrc}">
         <div>
-            <span class="brand-name">${brandName}</span>
-            <div class="product-name">${productName}</div>
+            <span class="brand-name">${item.brandName}</span>
+            <div class="product-name">${item.productName}</div>
         </div>
     </td>
     <td> 
@@ -66,16 +104,14 @@ function addItem(imageSrc, brandName, productName, price) {
         </form>  
     </td>
     <td>
-        <span class="shop-item-price">${price}</span>
+        <span class="shop-item-price">${item.price}</span>
         <form class="form" name="cart">
             <button class="cart-remove-btn" type="button" name="remove" onclick="">REMOVE</button>
         </form>   
     </td>`;
     cartRow.innerHTML = cartRowContents;
-    
-    $.get('cart.html', function () {
-        $('#test').append(cartRow);
-    });
+
+    return cartRow;
 }
 
 function updateTotal() {
@@ -100,6 +136,28 @@ function updateTotal() {
     total = Math.round(total * 100) / 100; // round the total price to 2 decimal places
     document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total; // display the total price in the cart
 }
+
+function reload() {
+
+    const rowsInTable = $('.cart-row')
+    
+    Object.keys(rowsInTable).forEach((key) => {
+        const item = rowsInTable[key];
+
+        if (item instanceof Element) {
+            rowsInTable[key].remove();
+        }
+    })
+    
+    cartItems.forEach((item) => {
+        const cartItem = generateCartItem(item);
+        $('.cart-table').find('tr:last').prev().after(cartItem)
+    })
+
+    updateTotal();
+}
+
+
 
 
 
