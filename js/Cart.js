bfc9f5b3615c9b809149923d1bf6c2f4b96c42da
rlicin7a <49114570+rlicin7a@users.noreply.github.com>
2019-04-12 17:31:34 +0800

    Update Cart.js

--- js/Cart.js
+++ js/Cart.js
@@ -1,17 +1,13 @@
 // Cart.js
-// load the body first before creating the Cart object
-// written by Ken
+// Collaborator: Ken
 
 var url = window.location.pathname;
 var filename = url.substring(url.lastIndexOf('/') + 1);
 
-// use getElementsByClassName() method to get a collection of elements having the name 'cart-remove-btn' in the page 
-// store all these elements in the array referenced by variable removeBtns
-var removeBtns = document.getElementsByClassName('cart-remove-btn');
-// then loop through each of the remove button in the removeBtns array
-for (var i=0; i<removeBtns.length; i++) {
-    var removeBtn = removeBtns[i];
-    removeBtn.addEventListener('click', removeItem); // when the remove button is clicked, call the removeItem function (event handler)
+var addBtns = document.getElementsByClassName('cart-add-btn');
+for (var i = 0; i<addBtns.length; i++) {
+    var addBtn = addBtns[i];
+    addBtn.addEventListener('click', fetchData); // fetch the data from the store after "Add to Cart" button is clicked
 }
 
 var qtyInputs = document.getElementsByClassName('cart-qty-input');
@@ -20,21 +16,30 @@ for (var i=0; i<qtyInputs.length; i++) {
     input.addEventListener('change', qtyValid); // check if the quantity is a valid number after quantity input is changed
 }
 
-var addBtns = document.getElementsByClassName('cart-add-btn');
-for (var i = 0; i<addBtns.length; i++) {
-    var addBtn = addBtns[i];
-    addBtn.addEventListener('click', fetchData); // fetch the data from the store after "Add to Cart" button is clicked
+/*
+// use getElementsByClassName() method to get a collection of elements having the name 'cart-remove-btn' in the page 
+// store all these elements in the array referenced by variable removeBtns
+var removeBtns = document.getElementsByClassName('cart-remove-btn');
+
+// then loop through each of the remove button in the removeBtns array
+for (var i=0; i<removeBtns.length; i++) {
+    var removeBtn = removeBtns[i];
+    removeBtn.addEventListener('click', removeItem); // when the remove button is clicked, call the removeItem function (event handler)
 }
+*/
 
 /**
  * cartItems is an array includes object that contains item information in the cart, and the element in cartItems is look like: 
- * const item = {
+ * const item = {   
         imageSrc,
         brandName,
         productName,
         price
     } 
 */ 
+
+// localStorage.clear(); // reset localStorage for different customers (a complete membership system will be made in the next version)
+
 var cartItems = JSON.parse(localStorage.getItem("CART_ITEMS"));
 
 // If it is null, which means CART_ITEMS is never stored, initialize it with an array
@@ -42,31 +47,14 @@ if (cartItems == null) {
     cartItems = []
 }
 
-// If user is in the cart page, run reload the generate the rows in the cart table
+// If user is in the cart page, run reload and generate the rows in the cart table
 if (filename === 'cart.html') {
     reload();
 }
 
-function removeItem(event) {
-    /**
-     * TODO: when removeItem(event) is clicked, you need to remove the corresponding item in cartItems
-     * Then, you can call reload() and the new table will be generated
-     * So, what you need to do is to find out the removeButton clicked is belong to which item and remove it in cartItems, then all things solve!
-     */
-}
-
-function qtyValid(event) {
-    input = event.target;
-    // make sure the quantity input is always a number greater than or equal to 1
-    if (isNaN(input.value) || input.value <= 0) {
-        input.value = 1;
-    }
-    updateTotal(); // update total price
-}
-
 // get the data of individual shop item that is added to cart
 function fetchData(event) {
-    var addBtn = event.target;
+    var addBtn = event.target; // the add button clicked is the event.target (object that triggers the event)
     var shopItem = addBtn.parentElement.parentElement.parentElement;
     var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src;
     var brandName = shopItem.getElementsByClassName('brand-name')[0].innerText;
@@ -80,16 +68,21 @@ function fetchData(event) {
         price
     }
 
-    cartItems.push(item)
+    cartItems.push(item);
     localStorage.setItem("CART_ITEMS", JSON.stringify(cartItems));
-
     // console.log(imageSrc, brandName, productName, price);
-    // addItem(imageSrc, brandName, productName, price);
 }
 
 function generateCartItem(item) {
     var cartRow = document.createElement('tr');
-    cartRow.className = "cart-row"
+    cartRow.className = "cart-row";
+    /*  
+    assign an unqiue id for each cartRow
+    the id is the same as the index of cartItems array
+    */
+    index = 0;
+    index = index + 1;
+
     var cartRowContents = `
     <td class="cart-item">
         <img class="cart-item-image" src="${item.imageSrc}">
@@ -103,15 +96,39 @@ function generateCartItem(item) {
             <input class="cart-qty-input" type="number" value="1">
         </form>  
     </td>
-    <td>
+    <td class="align-td">
         <span class="shop-item-price">${item.price}</span>
         <form class="form" name="cart">
-            <button class="cart-remove-btn" type="button" name="remove" onclick="">REMOVE</button>
+            <!-- pass the index directly to the function with the field onClick="removeItem(index)"-->
+            <button class="cart-remove-btn" type="button" name="remove" onclick="removeItem(index)">REMOVE</button>
         </form>   
     </td>`;
     cartRow.innerHTML = cartRowContents;
 
     return cartRow;
+}
+
+function qtyValid(event) {
+    input = event.target;
+    // make sure the quantity input is always a number greater than or equal to 1
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1;
+    }
+    updateTotal();
+}
+
+function removeItem(index) {
+    /**
+     * When removeItem(index) is clicked, remove the corresponding item in cartItems array
+     * Then, call reload() and the new table will be generated
+     * So, what we need to do is to find out the removeButton clicked is belong to which item and remove it in cartItems
+     * This can be done by making use of the index we set when generating the item
+     * Another approach is to use event listener? The event should tell the index of the row in the table
+     * and the index should be sync with the cartItems. We can actually assign an id to the row
+     * so as to use parentElement.getAttributeByName to obtain the index
+     */
+    cartItems.splice(index, 1);
+    reload();
 }
 
 function updateTotal() {
@@ -139,53 +156,20 @@ function updateTotal() {
 
 function reload() {
 
-    const rowsInTable = $('.cart-row')
+    const rowsInTable = $('.cart-row') // const means the variable is unchangable and the value assigned to it should be fixed
     
     Object.keys(rowsInTable).forEach((key) => {
-        const item = rowsInTable[key];
+        const item = rowsInTable[key]
 
         if (item instanceof Element) {
-            rowsInTable[key].remove();
+            rowsInTable[key].remove()
         }
     })
     
     cartItems.forEach((item) => {
-        const cartItem = generateCartItem(item);
+        const cartItem = generateCartItem(item)
         $('.cart-table').find('tr:last').prev().after(cartItem)
     })
 
     updateTotal();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
